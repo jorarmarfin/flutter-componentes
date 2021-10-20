518952cd9c4469b4cb2b5e8c1ac3106b532a9d01
Luis Mayta <luis.mayta@gmail.com>
2021-10-20 00:02:38 -0500

    Cargar un json

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
+
+import 'package:componentes/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         // debugShowCheckedModeBanner: false,
         title: 'Material App',
-        home: HomePageTemp());
+        home: HomePage());
   }
 }
--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['uno', 'dos', 'tres', 'cuatro'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Componente Temp'),
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('Titulo de lista'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Titulo de lista'),
-          )
-        ],
-      ),
+      body: ListView(children: _listado()),
     );
+  }
+
+  List<Widget> _listado() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item),
+            leading: Icon(Icons.ac_unit_outlined),
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
   }
 }
